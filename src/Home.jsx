498d3304a6a48b341dbaca7060658b2afab8c27d
BuildTools <unconfigured@null.spigotmc.org>
2023-05-21 00:55:39 -0600

    Actualizado pa Elian

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Home.css';
 import NavBar from './components/home/NavBar'
 import HomeDisplay from './components/home/HomeDisplay'
 import About from './components/about/About'
-
+import Notifications from './components/notifications/Notifications'
+import Panel from './components/panel/Panel'
 
 function Home({setShowLanding}) {
-  const [currentView, setCurrentView] = useSate('home');
+  const [currentView, setCurrentView] = useState('home');
 
-  const onClickChangeView = () => {
+  const onClickChangeView = (view) => {
     setCurrentView(view);
   }
-  
 
   return (
     <div id="content">
       <NavBar
       setShowLanding={setShowLanding}
+      onClickChangeView={onClickChangeView}
       />
-      <HomeDisplay/>
-      <About/>
-
+      {currentView === 'home' && <HomeDisplay/>}
+      {currentView === 'about' && <About/>}
+      {currentView === 'panel' && <Panel/>}
+      {currentView === 'notifications' && <Notifications/>}
     </div>
   );
 }
--- src/components/home/NavBar.jsx
+++ src/components/home/NavBar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './NavBar.css';
 
-function NavBar({setShowLanding}) {
+function NavBar({setShowLanding, onClickChangeView}) {
   const logOut = () => {
     localStorage.clear();
     setShowLanding(true);
-  }
+  };
+
+  const handleViewChange = (view) => {
+    onClickChangeView(view);
+  };
+
   return (
     <aside>
       <div className="navProfileContainer">
@@ -23,19 +28,19 @@ function NavBar({setShowLanding}) {
       </div>
 
       <div className="navContainer">
-        <a className="navIconContainer" href="adan.html">
+        <a className="navIconContainer" onClick={handleViewChange('home')}>
           <img className="navIcon" src="./src/assets/img/place.png" />
           <h4 className="navText">Catalogo</h4>
         </a>
-        <a className="navIconContainer" href="elian.html">
+        <a className="navIconContainer" onClick={handleViewChange('panel')}>
           <img className="navIcon" src="./src/assets/img/panel.png" />
           <h4 className="navText">Panel</h4>
         </a>
-        <a className="navIconContainer">
+        <a className="navIconContainer" onClick={handleViewChange('notifications')}>
           <img className="navIcon" src="./src/assets/img/not.png" />
           <h4 className="navText">Notificaciones</h4>
         </a>
-        <a className="navIconContainer" href="about.html">
+        <a className="navIconContainer" onClick={handleViewChange('about')}>
           <img className="navIcon" src="./src/assets/img/info.png" />
           <h4 className="navText">Conocenos</h4>
         </a>

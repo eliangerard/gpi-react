2b31e97ccd0c1c0247d2a63670df3d24e5add7a5
Elian <eliangerardiso@gmail.com>
2023-05-22 21:42:54 -0600

    Added questions fetch

---
+++ src/helpers/js/getPreguntas.js
@@ -0,0 +1,14 @@
+export const getPreguntas = async (token, id) => {
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + token);
+
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'manual'
+    };
+
+    return await fetch("https://208qra64eg.execute-api.us-east-2.amazonaws.com/dev/locaciones/preguntas?idLocacion="+id, requestOptions)
+        .then(response => response.json())
+
+}

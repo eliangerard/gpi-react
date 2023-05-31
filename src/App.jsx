f5853da7f909604038cb299bd3f409ae76102807
Elian <eliangerardiso@gmail.com>
2023-05-30 20:26:44 -0600

    Added auto login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react'
 import GPILanding from './GPILanding'
 import Home from './Home';
+import { getProfile } from './helpers/js/getProfile';
 
 export const App = () => {
     const [showLanding, setShowLanding] = useState(true);
 
-    const onLoad = () => {
-        if(localStorage.getItem("id"))
-            setShowLanding(false)
+    const onLoad = async () => {
+        if(localStorage.getItem("id")){
+            const response = await getProfile(localStorage.getItem("id"));
+            console.log(response);
+
+            return setShowLanding(response.message == "Access Denied")
+        }
+        return setShowLanding(true)
     }
 
     useEffect(() => {
-        //onLoad();
+        onLoad();
     }, []);
     
     return(

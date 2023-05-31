85a44fdad599dd66562de2ec790890754a486905
BuildTools <unconfigured@null.spigotmc.org>
2023-05-30 18:07:10 -0600

    no

--- src/components/panel/ImageUploadPreviewComponent.jsx
+++ src/components/panel/ImageUploadPreviewComponent.jsx
@@ -1,60 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
-import './Sliders.css';
+import './Sliders.css'
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const ImageUploadPreviewComponent = ({ handleImagesUploaded }) => {
-  const [fileArray, setFileArray] = useState([]);
+	const [fileArray, setFileArray] = useState([]);
 
-  useEffect(() => {
-    const fileURLs = fileArray.map((file) => URL.createObjectURL(file));
-    handleImagesUploaded(fileArray); // Actualizar el estado en el componente padre
+	const uploadMultipleFiles = (event) => {
+		console.log(event);
+		const fileArray = [...event.target.files];
+		const fileURLs = fileArray.map((file) => URL.createObjectURL(file));
+		const prevFileArray = [...fileURLs];
 
-    // Limpieza al desmontar el componente o al cambiar las imágenes
-    return () => {
-      fileURLs.forEach((url) => URL.revokeObjectURL(url));
-    };
-  }, [fileArray, handleImagesUploaded]);
+		let images64 = [];
+        fileArray.forEach(file => {
+			const reader = new FileReader();
+			reader.onloadend = async () => {
+				images64.push(reader.result);
+			};
+			reader.readAsDataURL(file);
+		});
 
-  const uploadMultipleFiles = (event) => {
-    const files = [...event.target.files];
-    setFileArray(files);
-  };
+		console.log(images64);
+		setFileArray(images64);
+		handleImagesUploaded(images64);
+	};
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	};
 
-  return (
-    <form>
-      <div className="form-group multi-preview">
-        <div className="contenedorSliderImageUpload">
-          <Slider {...settings}>
-            {fileArray.map((file, index) => (
-              <div key={index}>
-                <img src={URL.createObjectURL(file)} alt="Preview" className="image-preview" />
-              </div>
-            ))}
-          </Slider>
-        </div>
-      </div>
+	return (
+		<form>
+			<div className="form-group multi-preview">
+				<div className="contenedorSliderImageUpload">
+					<Slider {...settings}>
+						{fileArray.map((url, index) => (
+							<div key={index}>
+								<img src={url} alt="Preview" className="image-preview" />
+							</div>
+						))}
+					</Slider>
+				</div>
+			</div>
 
-      <div className="form-groupcontenedorSliderImageUpload">
-        <input
-          type="file"
-          className="form-control file-input"
-          onChange={uploadMultipleFiles}
-          multiple
-        />
-      </div>
-    </form>
-  );
+			<div className="form-groupcontenedorSliderImageUpload">
+				<input
+					type="file"
+					className="form-control file-input"
+					onChange={uploadMultipleFiles}
+					multiple
+				/>
+			</div>
+		</form>
+	);
 };
 
 export default ImageUploadPreviewComponent;
